9eaab8580c7635391ad9ca19c7349d076515aee5
Nick Volynkin <nick.volynkin@gmail.com>
2018-12-06 17:32:15 +0700

    add flake8, fix warnings

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""Code for the telegram-bot track."""
+
 import datetime
 import logging
 import os
@@ -15,18 +17,20 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 
 def main():
+    """Run the bot."""
     token = os.environ['TG_TOKEN']
     proxy = {'proxy_url': os.environ['PROXY_URL'],
              'urllib3_proxy_kwargs': {
                  'username': os.environ['PROXY_USER'],
-                 'password': os.environ['PROXY_PASS']}
+                 'password': os.environ['PROXY_PASS'],
+             },
              }
 
     bot = Updater(token, request_kwargs=proxy)
 
     dispatcher = bot.dispatcher
-    dispatcher.add_handler(CommandHandler("ping", greet_user))
-    dispatcher.add_handler(CommandHandler("planet", planet_command))
+    dispatcher.add_handler(CommandHandler('ping', greet_user))
+    dispatcher.add_handler(CommandHandler('planet', planet_command))
     dispatcher.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     bot.start_polling()
@@ -34,25 +38,27 @@ def main():
 
 
 def greet_user(bot: Bot, update: Update):
+    """React to /ping with 'pong'."""
     logging.info('command: /ping')
     update.message.reply_text('pong')
 
 
 def talk_to_me(bot: Bot, update: Update):
+    """Repeat user input."""
     user_text = update.message.text
-    logging.info('message: ' + user_text)
+    logging.info('message: {user_text}', extra={'user_text': user_text})
     update.message.reply_text(user_text)
 
 
 def planet_command(bot: Bot, update: Update):
-    logging.info('command: ' + update.message.text)
-
+    """Tell the constellation, that a planet is in at the moment."""
+    logging.info('command: {text}', extra={'text': update.message.text})
     answer = process_planet_command(update.message.text)
-
     update.message.reply_text(answer)
 
 
 def process_planet_command(text):
+    """Logic for planet_command."""
     args = text.split()
     if len(args) < 2:
         answer = "Add a planet's name, like this: /planet Mars"
@@ -60,10 +66,11 @@ def process_planet_command(text):
         planet_name = args[1]
         planet = get_planet(planet_name)
         if planet:
-            constell = get_constellation_name(planet)
-            answer = "{} is now in {}.".format(planet_name, constell)
+            # this returns a tuple like ('Aqr', 'Aquarius')
+            _, constell = ephem.constellation(planet)
+            answer = '{0} is now in {1}.'.format(planet_name, constell)
         else:
-            answer = "'{}' is not a planet.".format(planet_name)
+            answer = "'{0}' is not a planet.".format(planet_name)
     return answer
 
 
@@ -75,25 +82,16 @@ planets = {
     'saturn': ephem.Saturn,
     'uranus': ephem.Uranus,
     'neptune': ephem.Neptune,
-    'pluto': ephem.Pluto
+    'pluto': ephem.Pluto,
 }
 
 
-def get_planet(planet_name, time=datetime.datetime.now()):
+def get_planet(planet_name, time=None):
+    """Get the planets's ephem.Planet object by its name."""
     name_lower = planet_name.lower()
+    time = datetime.datetime.now() if time is None else time
 
     if name_lower in planets:
         return planets[name_lower](time)
     else:
         return None
-
-
-def get_constellation_name(planet):
-    # tuple like ('Aqr', 'Aquarius')
-    constell = ephem.constellation(planet)
-
-    return constell[1]
-
-
-if __name__ == '__main__':
-    main()
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Tests for the telegram-bot track.
+
+Run with ``python3 -m nose``.
+"""
+
 import bot
 
 
 def test_get_planet():
+    """Test that all planet names have assigned ephem objects."""
     planet_names = bot.planets.keys()
     for name in planet_names:
-        yield check_get_planet, name
+        yield _check_get_planet, name
 
 
-def check_get_planet(name):
-    # print(name)
-    print(bot.get_planet(name))
+def _check_get_planet(name):
     assert bot.get_planet(name) is not None
